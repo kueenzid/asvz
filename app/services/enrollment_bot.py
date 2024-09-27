12694617940ef686e61e2fb17c4961c7f1080421
Dennis Küenzi <dennis.kueenzi@gmx.ch>
2024-09-27 11:52:15 +0200

    Added logger

--- app/services/enrollment_bot.py
+++ app/services/enrollment_bot.py
@@ -3,6 +3,9 @@ import random
 import time
 from app.services import api_client
 from dateutil import parser
+from app.services.logger import setup_logger
+
+logger = setup_logger('enrollment_bot_logger', 'enrollment_bot')
 
 max_tries = 5
 delay = 0.2  # s
@@ -12,60 +15,58 @@ failure = []
 
 
 def enrollSpam(lesson_id):
-    print(
-        "Starting enrollment spam for lesson",
-        lesson_id,
-        "at ",
-        datetime.now(),
+    logger.info(
+        "Starting enrollment spam for lesson %s",
+        lesson_id
     )
     tries = 0
     while tries < max_tries:
         tries += 1
         response = enroll(lesson_id)
         if response.status_code == 201:
-            print("Successfully enrolled in lesson", lesson_id, "at ", datetime.now())
+            logger.info("Successfully enrolled in lesson %s", lesson_id)
             success.append(lesson_id)
             return
         else:
             time.sleep(delay)
 
-    print("Failed to enroll in lesson", lesson_id, "at ", datetime.now())
+    logger.error("Failed to enroll in lesson %s", lesson_id)
     still_trying.append(lesson_id)
     failure.append(lesson_id)
 
 
 def enroll(lesson_id):
-    print("Enrolling in lesson", lesson_id, "at ", datetime.now())
+    logger.info("Sending enrollment request for lesson %s", lesson_id)
     return api_client.enroll_in_lesson(lesson_id)
 
 
 def try_enrollment(lesson_id):
-    print("Check lesson", lesson_id, ":")
+    logger.info("Checking lesson %s", lesson_id)
     response = api_client.get_lesson(lesson_id)
     if response.status_code != 200:
         return False
 
     data = response.json()
     if data["data"]["participantsMax"] == data["data"]["participantCount"]:
-        print("Lesson", lesson_id, "is still full")
+        logger.info("Lesson %s is still full", lesson_id)
         return False
 
     cancelation_deadline = parser.parse(data["data"]["cancelationUntil"])
     if datetime.now(cancelation_deadline.tzinfo) > cancelation_deadline:
         still_trying.remove(lesson_id)
-        print("Removed lesson", lesson_id, "from still_trying because it is expired")
+        logger.info("Removed lesson %s from still_trying because it is expired", lesson_id)
         return False
 
     response = enroll(lesson_id)
     if response.status_code == 201:
-        print("Enrolled in lesson", lesson_id)
+        logger.info("Enrolled in lesson %s", lesson_id)
         failure.remove(lesson_id)
         still_trying.remove(lesson_id)
         success.append(lesson_id)
 
 
 def try_enrollment_for_still_trying():
-    print("Trying to enroll into full lessons, at ", datetime.now(), ":")
+    logger.info("Trying to enroll into full lessons")
     random_delay = random.randint(20, 60)
     time.sleep(random_delay)
     for lesson_id in still_trying:
